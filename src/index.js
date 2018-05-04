849bcd30186dca510cc370c0548cdbd6736668f8
robyhuzwandar <robyhuzwandar@gmail.com>
2018-05-04 14:09:52 +0800

    TODO List

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+var destination = document.querySelector("container");
+
+ReactDOM.render(
+    <div>
+        <p>HEllo </p>
+    </div>,
+    destination
+);
